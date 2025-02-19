f0b442f1374ce0c73dcb1924724bd399721952cf
JulieQvigstad <julieqvigstad@gmail.com>
2025-02-19 13:59:30 +0100

    flere endringer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const containerDiv = document.querySelector(".container")
 const figurElm = document.getElementById("figur")
 const hinderElm = document.getElementById("hinder")
+const ørnElm = document.getElementById("ørn")
 const startButton = document.getElementById("startButton")
 
 const maxLeft = containerDiv.offsetWidth - figurElm.offsetWidth
@@ -8,20 +9,19 @@ const maxTopp = containerDiv.offsetHeight - figurElm.offsetHeight
 const hinderStart = maxLeft + 500
 
 const looseSound = document.getElementById("looserSound")
-const lydSpiller = new Audio("looser.mp3")
+//const lydSpiller = new Audio("looser.mp3")
 
 
 let y = maxTopp //start nederst y=katt
 let vy = 0 //fart i y retning (opp og ned)
-let x = hinderStart //Hinderets startsposisjon
-const vx = -5 //Hastighet til hinderet 
+let buskX = hinderStart //buskens startsposisjon
+let ørnX = hinderStart + 600 //ørnen starter litt bak busken
+const vx = -5 //Hastighet til hinderne  
 
 const GRAVITASJON = 1
-let spillAktivt = false //Spillet starter ikke aktivt
+//let spillAktivt = false //Spillet starter ikke aktivt
 
 function hopp() {
-    if (!spillAktivt) return
-    
     vy += GRAVITASJON //gravitasjonen påvirker farten i y-retning (faller ned)
     y += vy //y-posisjon endrer seg med y-fart
     figurElm.style.top = y + "px"
@@ -36,8 +36,8 @@ function hopp() {
 }
 
 function tasteTrykk(event) {
-    if (event.key == " " && spillAktivt) {
-        //console.log("Du presset space")
+    if (event.key == " ") {
+        console.log("Du presset space")
         vy = -20
     }
 
@@ -50,14 +50,16 @@ document.addEventListener("keypress", tasteTrykk)
 //const vx = -5 // Farten til hinderne
 
 function oppdaterHindre() {
-    if (!spillAktivt) return
-    
-    x += vx
-    hinderElm.style.left = x + "px"
-    
-    if (x < -hinderElm.offsetWidth) {
-        x = hinderStart 
-        hinderElm.style.bottom = 100 + "px"
+    buskX += vx
+    hinderElm.style.left = buskX + "px"
+
+    ørnX += vx
+    ørnElm.style.left = ørnX + "px"
+
+    if (buskX < -hinderElm.offsetWidth) {
+        buskX = hinderStart
+        //hinderElm.style.bottom = 100 + "px"
+        ørnX = buskX + 400
     }
 }
 
@@ -65,21 +67,24 @@ function oppdaterHindre() {
 function sjekkKollisjon() {
     const figurRect = figurElm.getBoundingClientRect() //får info om posisjonen og størrelsen til figur elementet
     const hinderRect = hinderElm.getBoundingClientRect() //samme 
+    const ørnRect = ørnElm.getBoundingClientRect()
     const buffer = 50 //gjør at kollisjonen skjer når man ser at de kolliderer 
 
     return (
-        figurRect.right > hinderRect.left + buffer &&
+        (figurRect.right > hinderRect.left + buffer &&
         figurRect.left < hinderRect.right - buffer &&
         figurRect.bottom > hinderRect.top + buffer &&
-        figurRect.top < hinderRect.bottom - buffer
-    ) 
+        figurRect.top < hinderRect.bottom - buffer) ||
+
+        (figurRect.right > ørnRect.left + buffer &&
+        figurRect.left < ørnRect.right - buffer &&
+        figurRect.bottom > ørnRect.top + buffer &&
+        figurRect.top < ørnRect.bottom - buffer)
+    )
 }
 
 //Funksjon for å vise "GAME OVER"
 function visGameOver() {
-    spillAktivt = false
-    
-
     const gameOverText = document.createElement("h1")
     gameOverText.innerText = "GAME OVER"
     gameOverText.style.position = "absolute"
@@ -92,9 +97,12 @@ function visGameOver() {
     gameOverText.style.fontFamily = "Press Start 2P"
     gameOverText.style.padding = "20px"
     gameOverText.style.borderRadius = "10px"
-    
+
     document.body.appendChild(gameOverText)
-    looseSound.play();
+
+    if (looseSound) {
+        looseSound.play()
+    }
 
     setTimeout(() => {
         startButton.style.display = "block" // Viser startknappen igjen
@@ -102,8 +110,10 @@ function visGameOver() {
     }, 2000) //tiden det tar før teksten kommer 
 }
 
+let spillAktivt = false 
+
 //Starter spillet på nytt
-function startSpill() {
+/*function startSpill() {
     spillAktivt = true
 
     y = maxTopp // Nullstiller figurens posisjon
@@ -116,6 +126,7 @@ function startSpill() {
     startButton.style.display = "none" // Skjuler startknappen
     requestAnimationFrame(oppdaterAlt) // Starter spill-loop
 }
+*/
 
 
 //let spillAktivt = true
@@ -127,7 +138,7 @@ function oppdaterAlt() {
     oppdaterHindre()
 
     if (sjekkKollisjon()) {
-        //spillAktivt = false
+        spillAktivt = false
         console.log("Kollisjon! Spillet stopper.")
         visGameOver() //Viser skriften "game over"
     } else {
@@ -135,13 +146,27 @@ function oppdaterAlt() {
     }
 }
 
-startButton.addEventListener("click", startSpill)
+//startButton.addEventListener("click", startSpill)
 
-/*startButton.addEventListener("click", () => {
+startButton.addEventListener("click", () => {
     spillAktivt = true
     startButton.style.display = "none" // Skjuler startknappen
+
+    y = maxTopp
+    vy = 0
+    buskX = hinderStart
+    ørnX = hinderStart + 600
+
+    figurElm.style.top = y + "px"
+    hinderElm.style.left = buskX + "px"
+    ørnElm.style.left = ørnX + "px"
+
     oppdaterAlt()
-})*/
+    
+    //buskX = hinderStart
+    //ørnX = hinderStart + 400
+
+})
 
 
 
